6de936ac28cbbaf82c6f2ac7979d597d4d695196
GingBaoNo <gingbao26062003@gmail.com>
2024-10-14 14:21:19 +0700

    update

--- src/main/java/com/example/motogp/Rider/RiderRepository.java
+++ src/main/java/com/example/motogp/Rider/RiderRepository.java
@@ -3,11 +3,7 @@ package com.example.motogp.Rider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.motogp.Team.Team;
-
 import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface RiderRepository extends JpaRepository<Rider, Integer> {
     // Tìm kiếm rider theo tên
